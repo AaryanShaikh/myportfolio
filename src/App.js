7afdc2c798b8f88b513428ba6a2fa8ebc5fc8298
Aaryan Shaikh <aayanshaikh27638@gmail.com>
2021-11-05 20:17:04 +0530

    init

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom'
 
 export default class App extends Component {
   constructor() {
@@ -62,30 +62,28 @@ export default class App extends Component {
     } else {
       return (
         <div className="App">
-          <Router>
-            <div className="header">
-              <div className="logo">
-                <img src="https://raw.githubusercontent.com/AaryanShaikh/badportfolio/main/public/logo.png" alt="" onMouseOver={this.makeCircleBorder} onMouseOut={this.remCircleBorder} />
-              </div>
-              <div className={this.state.isHamActive ? "nav" : "nav hide"}>
-                <Link to="/" onMouseOver={this.makeCircleVanish} onMouseOut={this.makeCircleAppear}><p><span>H</span>ome</p></Link>
-                <Link to="/about" onMouseOver={this.makeCircleVanish} onMouseOut={this.makeCircleAppear}><p><span>A</span>bout</p></Link>
-                <Link to="/projects" onMouseOver={this.makeCircleVanish} onMouseOut={this.makeCircleAppear}><p><span>P</span>rojects</p></Link>
-                <Link to="/contact" onMouseOver={this.makeCircleVanish} onMouseOut={this.makeCircleAppear}><p><span>C</span>ontact</p></Link>
-              </div>
-              <ul onClick={this.hamToggle} className={this.state.isHamActive ? "active" : ""} onMouseOver={this.makeCircleBigger} onMouseOut={this.makeCircleSmall}>
-                <li></li>
-                <li></li>
-                <li></li>
-              </ul>
+          <div className="header">
+            <div className="logo">
+              <img src="https://raw.githubusercontent.com/AaryanShaikh/badportfolio/main/public/logo.png" alt="" onMouseOver={this.makeCircleBorder} onMouseOut={this.remCircleBorder} />
             </div>
-            <Switch>
-              <Route exact path="/"><Home /></Route>
-              <Route path="/about"><About /></Route>
-              <Route path="/projects"><Projects /></Route>
-              <Route path="/contact"><Contact /></Route>
-            </Switch>
-          </Router>
+            <div className={this.state.isHamActive ? "nav" : "nav hide"}>
+              <Link to="/" onMouseOver={this.makeCircleVanish} onMouseOut={this.makeCircleAppear}><p><span>H</span>ome</p></Link>
+              <Link to="/about" onMouseOver={this.makeCircleVanish} onMouseOut={this.makeCircleAppear}><p><span>A</span>bout</p></Link>
+              <Link to="/projects" onMouseOver={this.makeCircleVanish} onMouseOut={this.makeCircleAppear}><p><span>P</span>rojects</p></Link>
+              <Link to="/contact" onMouseOver={this.makeCircleVanish} onMouseOut={this.makeCircleAppear}><p><span>C</span>ontact</p></Link>
+            </div>
+            <ul onClick={this.hamToggle} className={this.state.isHamActive ? "active" : ""} onMouseOver={this.makeCircleBigger} onMouseOut={this.makeCircleSmall}>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+          <Switch>
+            <Route exact path="/"><Home /></Route>
+            <Route path="/about"><About /></Route>
+            <Route path="/projects"><Projects /></Route>
+            <Route path="/contact"><Contact /></Route>
+          </Switch>
         </div>
       );
     }
